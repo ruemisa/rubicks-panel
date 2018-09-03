201e76b56865e614fe10449b27a59394739d8c90
Ruel Alfonso Misa <ruel.g.misa@gmail.com>
2018-09-03 15:38:42 -0400

    Added the main pages to application

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  # Other MAIN pages
+  get 'about-us' => 'pages#about'
+  get 'contact-us' => 'pages#contact'
+
+  # HOME
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
