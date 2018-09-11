07d3e67a26562c67247a649d9624af617f4ba990
Ruel Alfonso Misa <ruel.g.misa@gmail.com>
2018-09-11 19:46:03 -0400

    finished styling for layout and home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
   def login_helper
     if current_user.is_a?(User)
+      (link_to "Dashboard", dashboard_path ) +
+      "<li>".html_safe +
       (link_to "Logout", destroy_user_session_path, method: :delete) +
-      "<br>".html_safe +
-      (link_to "Dashboard", dashboard_path )
+      "<li>".html_safe
     else  
       link_to "Login", new_user_session_path
     end
@@ -14,7 +15,6 @@ module ApplicationHelper
   end
 
 end
-
 
 # Just in case I want users to register. Don't forget the () wrap around login link
 # (link_to "Register", new_user_registration_path) + "<br>".html_safe +
